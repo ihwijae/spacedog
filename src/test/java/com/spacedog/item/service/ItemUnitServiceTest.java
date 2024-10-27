04cca7cbce6bdb48b2562df0d96b36167e43a8c4
ihwijae <lhj851@gmail.com>
2024-10-27 20:54:49 +0900

    상품서비스 단위테스트

--- src/test/java/com/spacedog/item/service/ItemUnitServiceTest.java
+++ src/test/java/com/spacedog/item/service/ItemUnitServiceTest.java
@@ -4,28 +4,29 @@ import com.spacedog.cart.repository.CartRepository;
 import com.spacedog.category.service.CategoryService;
 import com.spacedog.item.domain.Item;
 import com.spacedog.item.dto.*;
+import com.spacedog.item.exception.NotEnoughStockException;
 import com.spacedog.item.repository.ItemRepository;
 import com.spacedog.item.repository.ItemRepositoryPort;
 import com.spacedog.member.domain.Member;
 import com.spacedog.member.repository.MemberRepository;
 import com.spacedog.member.service.MemberService;
+import com.spacedog.member.service.MemberValidate;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -34,46 +35,148 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @Slf4j
 public class ItemUnitServiceTest {
 
-    private MemberService memberService;
+    private ItemRepositoryPort itemRepositoryPort = mock(ItemRepositoryPort.class);
+    private MemberService memberService = mock(MemberService.class);
+    private CategoryService categoryService = mock(CategoryService.class);
+    private OptionService optionService = mock(OptionService.class);
     private ItemService itemService;
-    private ItemRepositoryPort itemRepository;
-    private BCryptPasswordEncoder passwordEncoder;
-    private CartRepository cartRepository;
-    private MemberRepository memberRepository;
+
+    @BeforeEach
+            public void setup() {
+        itemService = new ItemService(memberService, categoryService, optionService, itemRepositoryPort);
+//        SecurityContext context = SecurityContextHolder.createEmptyContext();
+//        TestingAuthenticationToken mockAuthentication = new TestingAuthenticationToken("lhj@naver.com", "12345678");
+//        context.setAuthentication(mockAuthentication);
+//        SecurityContextHolder.setContext(context);
+    }
+
+    @Test
+    void 판매자는_상품을_등록할수_있다() {
+
+        //given
+        Member member = Member.builder()
+                .email("test@email.com")
+                .id(1L)
+                .build();
+
+        CreateItemRequest createItemRequest = CreateItemRequest.builder()
+                .id(3L)
+                .name("testItem")
+                .price(9999)
+                .build();
 
 
-//    @BeforeEach
-//            public void setup() {
-//        memberRepository = new MemberRepository();
-//        memberService = new MemberService()
-//        itemRepository = new MockItemRepository();
-//        itemService = new ItemService()
-//    }
+        when(memberService.getMember()).thenReturn(member);
+        when(itemRepositoryPort.existByName(any())).thenReturn(false);
+        when(itemRepositoryPort.save(any(Item.class))).thenReturn(Item.builder().id(3L).name("saveTestItem").build());
 
-//    @Mock
-//    private ItemService itemService;
-//    @Mock
-//    private ItemRepository itemRepository;
-//    @Mock
-//    private CategoryService categoryService;
-//    @Mock
-//    private MemberService memberService;
-//    @Mock
-//    private MemberRepository memberRepository;
-//
-//
-//
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
+
+        //when
+        Long saveId = itemService.createItem(createItemRequest);
+
+
+        //then
+        assertThat(saveId).isEqualTo(3L);
+    }
+
+    @Test
+    void 상품이_중복되면_등록시_예외가_발생한다() {
+
+        //given
+        Member member = Member.builder()
+                .email("test@email.com")
+                .id(1L)
+                .build();
+
+        CreateItemRequest createItemRequest = CreateItemRequest.builder()
+                .id(3L)
+                .name("testItem")
+                .price(9999)
+                .build();
+
+
+
+        when(memberService.getMember()).thenReturn(member);
+        when(itemRepositoryPort.existByName(any())).thenReturn(true);
+        when(itemRepositoryPort.save(any(Item.class))).thenReturn(Item.builder().id(3L).build());
+
+
+        //when then
+      assertThrows(NotEnoughStockException.ItemDuplicate.class, () -> {
+          itemService.createItem(createItemRequest);
+      });
+
+
+    }
+
+    @Test
+    void 상품엔티티에서_상품을_생성할수있다() {
+
+        //given
+        CreateItemRequest createItemRequest = CreateItemRequest.builder()
+                .name("testItem")
+                .price(9999)
+                .build();
+
+        //when
+        Item item = Item.createItem(createItemRequest, false);
+
+        //then
+        assertThat(item.getName()).isEqualTo("testItem");
+    }
+
+    @Test
+    void 옵션이_있으면_옵션을_저장하는_메서드를_실행한다() {
+
+        //given
+        Member member = Member.builder()
+                .email("test@email.com")
+                .id(1L)
+                .build();
+
+
+
+        OptionSpecsRequest testOptionSpecs = OptionSpecsRequest.builder()
+                .name("testOptionSpecs")
+                .stockQuantity(190)
+                .build();
+
+        OptionGroupRequest testOptionGroup = OptionGroupRequest.builder()
+                .name("testOptionGroup")
+                .optionSpecsRequest(List.of(testOptionSpecs))
+                .build();
+
+        CreateItemRequest createItemRequest = CreateItemRequest.builder()
+                .id(2L)
+                .name("testItem")
+                .price(9999)
+                .optionGroups(List.of(testOptionGroup))
+                .build();
+
+
+        //when
+        given(memberService.getMember()).willReturn(member);
+        given(itemRepositoryPort.existByName(any())).willReturn(false);
+        given(itemRepositoryPort.save(any(Item.class))).willReturn(Item.builder().id(createItemRequest.getId()).build());
+        itemService.createItem(createItemRequest);
+
+
+        // then
+        verify(optionService, times(1)).saveOptionWithItem(any(CreateItemRequest.class), any(Item.class));
+
+    }
+
+
+
 //
 //
 //    @Test
@@ -118,73 +221,6 @@ public class ItemUnitServiceTest {
 //    }
 
 
-    // mock 없이 test
-    void 아이템생성 () {
-
-
-    }
-
-    static class MockItemRepository implements ItemRepositoryPort {
-
-        private final AtomicLong autoGeneratedId = new AtomicLong();
-
-
-
-        @Override
-        public Optional<Item> findById(Long id) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Item save(Item item) {
-            return null;
-        }
-
-        @Override
-        public void delete(Long id) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<Item> items) {
-
-        }
-
-        @Override
-        public void update(Long itemId, ItemEditRequest request) {
-
-        }
-
-        @Override
-        public Optional<Item> findByName(String name) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existByName(String name) {
-            return false;
-        }
-
-        @Override
-        public List<SearchItemResponse> getItems(SearchItemRequest request) {
-            return List.of();
-        }
-
-        @Override
-        public List<FindItemAllResponse> findItemsAll(int pageNo, int pageSize) {
-            return List.of();
-        }
-
-        @Override
-        public Optional<Item> findByItemWithCategory(Long id) {
-            return Optional.empty();
-        }
-
-        @Override
-        public List<ItemDetailResponse> itemDetail(Long itemId) {
-            return List.of();
-        }
-    }
 
 
 
